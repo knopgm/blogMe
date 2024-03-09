699c0f9c1397b38436f104f605a3d50f08b1215d
knopgm <knop.gm@gmail.com>
2024-03-09 18:13:29 +0100

    feat: Create AppRouter and connect Home page to the main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { AppRoutes } from './routes/AppRoutes';
+import Home from './pages/Home/Home';
+
 import './App.css';
 
 function App() {
+  const Layout = () => {
+    return (
+      <div id="root" className="app_wrapper">
+        <Outlet />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: AppRoutes.MAIN,
+        element: <Layout />,
+        errorElement: <h1>PAGE NOT FOUND</h1>,
+        children: [
+          {
+            path: AppRoutes.MAIN,
+            element: <Home />,
+            errorElement: <h1>HOME PAGE NOT FOUND</h1>,
+          },
+        ],
+      },
+    ],
+    { basename: '/' }
+  );
+
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
