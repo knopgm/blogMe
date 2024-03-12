5995a7db0ef92ea02993cf17fbc55bd59d5b61cf
knopgm <knop.gm@gmail.com>
2024-03-12 22:12:41 +0100

    feat: Create a Hero component

--- src/pages/BlogAllArticles/BlogAllArticles.tsx
+++ src/pages/BlogAllArticles/BlogAllArticles.tsx
@@ -1,27 +1,21 @@
+import Hero from '../../components/Hero/Hero';
 import './BlogAllArticles.css';
 
 const BlogAllArticles = () => {
   return (
-    <div className="headerAllArticles">
-      <header>
-        <h1>BloG-ME</h1>
-        <div className="subContentOfheaderAllArticles">
-          <h2 className="subContentOfheaderAllArticles_Title">
-            Express yourself!
-          </h2>
-          <p
-            /* ref={headerSectionRef} */ className="headerAllArticles_Paragraph"
-          >
-            A ready to go app for your articles and opinions
-          </p>
+    <div>
+      <div className="headerAllArticles">
+        <Hero />
+      </div>
+
+      <body className="bodyAllArticles">
+        <div className="bodyAllArticles_contentWrapper">
+          <h1 className="bodyAllArticles_Title">My Articles</h1>
+          <div className="bodyAllArticles_CardsWrapper">
+            <div className="articlesCardsGrid">Article Components here</div>
+          </div>
+          <h2>Pagination Component goes here</h2>
         </div>
-      </header>
-      <body>
-        <h1>My Articles</h1>
-        <div className="bodyAllArticlesWrapper">
-          <h2>Map through Articles cards Component here</h2>
-        </div>
-        <h2>Pagination Component goes here</h2>
       </body>
     </div>
   );
