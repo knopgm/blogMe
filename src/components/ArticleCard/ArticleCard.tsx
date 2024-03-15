49f15beff562bd2eb328966bacba08796bb6f808
knopgm <knop.gm@gmail.com>
2024-03-15 14:57:34 +0100

    feat: Create ArticleCard and map it to render all article cards

---
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import TextTruncate from 'react-text-truncate';
+
+import styles from './ArticleCard.module.css';
+
+type ArticleCardProps = {
+  image: string;
+  title: string;
+  readTime: string;
+  publishedDate: string;
+  shortDescription: string;
+  readMoreUrl: string;
+};
+
+const ArticleCard = ({
+  image,
+  title,
+  readTime,
+  publishedDate,
+  shortDescription,
+  readMoreUrl,
+}: ArticleCardProps) => {
+  return (
+    <div>
+      <article className={styles.articleCard} data-testid="article-card">
+        <div className={styles.cardImage}>
+          <img src={image} alt="article-image" />
+        </div>
+        <div className={styles.articleContent}>
+          <div className={styles.article}>
+            <h1
+              className={styles.articleTitle}
+              data-testid="article-card-title"
+            >
+              {title}
+            </h1>
+            <div className={styles.articlesInfo}>
+              <div>{readTime} read</div> <div>{publishedDate}</div>
+            </div>
+            {/* You can change how many lines the cards will display at line=() below */}
+
+            <TextTruncate line={3} element="p" text={shortDescription} />
+          </div>
+          <Link to={readMoreUrl}>
+            <button className={styles.btnReadMore}>READ MORE</button>
+          </Link>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- src/pages/BlogAllArticles/BlogAllArticles.tsx
+++ src/pages/BlogAllArticles/BlogAllArticles.tsx
@@ -1,22 +1,152 @@
-import Hero from '../../components/Hero/Hero';
-import './BlogAllArticles.css';
+// import type { PropsWithChildren } from 'react';
+import { useEffect, useState, useRef } from 'react';
+// import Hero from '../../components/Hero/Hero';
+import ArticleCard from '../../components/ArticleCard/ArticleCard';
+// import Pagination from '../../components/Pagination/Pagination';
+import { ARTICLES } from '../../utilities/articles/data';
+import type { Article } from '../../utilities/articles/types';
 
-const BlogAllArticles = () => {
+import styles from './BlogAllArticles.module.css';
+
+// const x: Article[] = [];
+// const x: Array<Article> = [];
+
+// This function slices the list of articles to a list hanging on the number of
+function getArticlesForPage(
+  pageNumber: number,
+  articlesPerPage: number,
+  allCardArticlesData?: Array<Article>
+) {
+  const pageFirstElementIndex = articlesPerPage * (pageNumber - 1);
+  if (!allCardArticlesData) {
+    return;
+  }
+  return allCardArticlesData.slice(
+    pageFirstElementIndex,
+    pageFirstElementIndex + articlesPerPage
+  );
+}
+
+type BlogAllArticlesProps = {
+  articles?: Array<Article>;
+  articlesPerPage?: number;
+  initialPage: number;
+};
+
+/* Example ------------------ */
+// type NameComponentProps = {
+//   name?: string;
+// };
+// type LastNameComponentProps = {
+//   lastName?: string;
+// };
+
+// const DEFAULT_NAME = 'Gabriela';
+// const name = 'Gabriela';
+// const lastName = 'Knop';
+
+// const obj = { name };
+
+// const LastNameComponent = ({ lastName }: LastNameComponentProps) => {
+//   return <p>{lastName}</p>;
+// };
+
+// const NameComponent = ({ name = DEFAULT_NAME }) => {
+//   return <p>{name}</p>;
+// };
+
+/* Example ------------------ */
+
+const BlogAllArticles = ({
+  articles = ARTICLES,
+  articlesPerPage = 3,
+  initialPage = 1,
+}: BlogAllArticlesProps) => {
+  /* to render Example */
+  // return (
+  //   <>
+  //     <div>Blog All Articles</div>
+  //   </>
+  // );
+  /* to render Example */
+
+  // // Temporarely hard coded state -> to be fetch from a server in the future
+  // const [articlesCardList, setArticlesCardList = useState(articles);
+  const articlesCardList = articles;
+
+  console.log(articlesCardList);
+  console.log(articlesPerPage);
+  console.log(initialPage);
+
+  const [currentPage, setCurrentPage] = useState(initialPage);
+  // // a ref to be used on the scrollIntoView method
+  // const headerSectionRef = useRef(null);
+  // calls the slice function to have a default list of cards on the page
+  const [displayedArticlesList, setDisplayedArticlesList] = useState(() => {
+    return getArticlesForPage(currentPage, articlesPerPage, articlesCardList);
+  });
+
+  useEffect(() => {
+    // Will call the slice function again to set the correspondent list of articles with if a new page is selected
+    // the displayArticlesList will be mapped and displayed inside the render to render the article Cards
+    setDisplayedArticlesList(
+      getArticlesForPage(currentPage, articlesPerPage, articlesCardList)
+    );
+  }, [currentPage, articlesCardList, articlesPerPage]);
+
+  // // logic to set how many buttons/pages should be displayed on the pagination
+  const numberOfPages = Math.ceil(articlesCardList.length / articlesPerPage);
+
+  // When User clicks on another page it will...
+  const handlePaginationChange = (pageNumber: number) => {
+    if (currentPage <= 0 || currentPage > numberOfPages) {
+      return;
+    }
+
+    // Smothly scroll the page up
+    // headerSectionRef.current
+    //   ? headerSectionRef.current?.scrollIntoView()
+    //   : 'undefined';
+    // headerSectionRef.current?.scrollIntoView();
+
+    // Set the number of the page
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
-      <div className="headerAllArticles">
-        <Hero />
+      <div className={styles.headerAllArticles}>
+        <h1>hello world</h1>
+        {/* <Hero headerSectionRef={headerSectionRef} /> */}
       </div>
 
-      <body className="bodyAllArticles">
-        <div className="bodyAllArticles_contentWrapper">
-          <h1 className="bodyAllArticles_Title">My Articles</h1>
-          <div className="bodyAllArticles_CardsWrapper">
-            <div className="articlesCardsGrid">Article Components here</div>
+      <div className={styles.bodyAllArticles}>
+        <h1 className={styles.bodyAllArticles_Title}>More Articles</h1>
+        <div className={styles.bodyAllArticlesWrapper}>
+          <div className={styles.articlesCardsGrid}>
+            <div className={styles.articlesCardsGrid}>
+              {displayedArticlesList?.map((article) => {
+                return (
+                  <ArticleCard
+                    key={article.key}
+                    image={article.image}
+                    title={article.title}
+                    readTime={article.infos.readTime}
+                    publishedDate={article.infos.publishedDate}
+                    shortDescription={article.shortDescription}
+                    readMoreUrl={article.readMoreUrl}
+                  />
+                );
+              })}
+            </div>
           </div>
-          <h2>Pagination Component goes here</h2>
+          {/* <Pagination
+            currentPage={currentPage}
+            numberOfPages={numberOfPages}
+            onPaginationChange={handlePaginationChange}
+          /> */}
         </div>
-      </body>
+      </div>
     </div>
   );
 };
