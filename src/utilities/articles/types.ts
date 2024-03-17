0bfbbfd92feb171dd0a01a48fb9994e10bb2d7f5
knopgm <knop.gm@gmail.com>
2024-03-17 15:16:14 +0100

    feat: Create ArticleDetails type

--- src/utilities/articles/types.ts
+++ src/utilities/articles/types.ts
@@ -12,3 +12,62 @@ export type Article = {
   shortDescription: string;
   readMoreUrl: string;
 };
+
+type ArticleDetailsContentPlain = {
+  type: 'plain';
+  title?: string;
+  body: string;
+};
+
+type ArticleDetailsContentImage = {
+  type: 'image';
+  sources: {
+    desktop: string;
+    mobile: string;
+  };
+};
+
+type ArticleDetailsContentList = {
+  type: 'list';
+  title?: string;
+  body?: string;
+  subSections: Array<{
+    title: string;
+    body: string;
+  }>;
+};
+
+type ArticleDetailsContentListWithBulletPoints = {
+  type: 'listWithBulletPoints';
+  title?: string;
+  body?: string;
+  subSections: Array<{
+    title: string;
+    body: string;
+  }>;
+};
+
+type ArticleDetailsContent =
+  | ArticleDetailsContentPlain
+  | ArticleDetailsContentImage
+  | ArticleDetailsContentList
+  | ArticleDetailsContentListWithBulletPoints;
+
+export type ArticleDetails = {
+  id: number;
+  key: string;
+  navbarBackground: boolean;
+  title: string;
+  heroImg: {
+    desktop: string;
+    mobile: string;
+  };
+  metadata: {
+    readTime: string;
+    date: string;
+  };
+  highlight: {
+    body: string;
+  };
+  content: Array<ArticleDetailsContent>;
+};
