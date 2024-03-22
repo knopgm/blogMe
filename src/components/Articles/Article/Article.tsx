81c072ebcf9e2101735cabbedf2c69f2aa2af57f
knopgm <knop.gm@gmail.com>
2024-03-22 17:17:37 +0100

    feat: Create a ReadNextArticleCard and connect with the article component

--- src/components/Articles/Article/Article.tsx
+++ src/components/Articles/Article/Article.tsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
-import { useNavigate, useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { articles } from '../articles';
-// import { articlesData } from '../../../utilities/articles/data';
+import { ARTICLES } from '../../../utilities/articles/data';
 import Hero from './components/Hero/Hero';
 import HighlightedSection from './components/HighlightedSection/HighlightedSection';
 import PlainSection from './components/PlainSection/PlainSection';
 import Image from './components/Image/Image';
 import SectionList from './components/SectionList/SectionList';
+import SectionListWithBulletPoints from './components/SectionListWithBulletPoints/SectionListWithBulletPoints';
+import ReadNextArticleCard from '../../ReadNextArticleCards/ReadNextArticleCards';
+import { getNextArticles } from '../../../utilities/getNextArticles';
 
 import styles from './Articles.module.css';
-import SectionListWithBulletPoints from './components/SectionListWithBulletPoints/SectionListWithBulletPoints';
 
 const Article = () => {
   // Will read the path from the URL, that is the same as the key property
@@ -18,19 +19,15 @@ const Article = () => {
 
   const articleName = key;
 
-  // Save the specific search Param (in this case: the article name) in the list of articles
-  // and use this variable to render all articles dynamically
+  // Save the specific search Param (in this case: the article name) in the
+  // list of articles and use this variable to render all articles dynamically
   const article = articleName ? articles[articleName] : undefined;
-
-  // useEffect(() => {
-  //   if (!article) {
-  //     navigate('/blog/all');
-  //   }
-  // }, [article, navigate]);
 
   if (!article) {
     return null;
   }
+
+  const nextArticles = getNextArticles(article.id, ARTICLES);
 
   return (
     <div data-testid={`article-wrapper`}>
@@ -97,6 +94,29 @@ const Article = () => {
           })}
         </div>
       </article>
+      <div className={styles.whatToReadNext_wrapper}>
+        <h1
+          data-testid="whatToReadNext_title"
+          className={styles.whatToReadNext_title}
+        >
+          What to read next
+        </h1>
+        <div className={styles.whatToReadNextCards_wrapper}>
+          {nextArticles.map((article) => {
+            return (
+              <ReadNextArticleCard
+                key={article.id}
+                image={article.image}
+                title={article.title}
+                readTime={article.infos.readTime}
+                publishedDate={article.infos.publishedDate}
+                text={article.shortDescription}
+                readMoreUrl={article.readMoreUrl}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/ReadNextArticleCards/ReadNextArticleCards.tsx
@@ -0,0 +1,48 @@
+import TextTruncate from 'react-text-truncate';
+import { Link } from 'react-router-dom';
+import CardInfos from '../CardInfos/CardInfos';
+
+import styles from './ReadNextArticleCard.module.css';
+
+type ReadNextArticleCardProps = {
+  image: string;
+  title: string;
+  readTime: string;
+  publishedDate: string;
+  text: string;
+  readMoreUrl: string;
+};
+
+const ReadNextArticleCard = ({
+  image,
+  title,
+  readTime,
+  publishedDate,
+  text,
+  readMoreUrl,
+}: ReadNextArticleCardProps) => {
+  return (
+    <article className={styles.readNextCard} data-testid="article-card">
+      <div className={styles.readNextCardImage}>
+        <img src={image} alt="article-image" />
+      </div>
+      <div className={styles.readNextCardContent}>
+        <div className={styles.readNextCardContent_grid}>
+          <h1 className={styles.readNextCardTitle}>{title}</h1>
+          <CardInfos readTime={readTime} publishedDate={publishedDate} />
+          {/* You can change how many lines the cards will display at line=() below */}
+          <TextTruncate line={4} element="p" text={text} />
+        </div>
+        <div className={styles.readNextCardBtnReadMore_wrapper}>
+          <Link to={readMoreUrl}>
+            <button className={styles.readNextCardBtnReadMore}>
+              READ MORE
+            </button>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ReadNextArticleCard;
